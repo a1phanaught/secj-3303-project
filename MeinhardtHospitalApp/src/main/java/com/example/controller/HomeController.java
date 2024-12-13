8e017a11f8a6a6dd9367e9efe98a877e32a55423
HAZIQ AZLIM <haziq.azlim@wdc.com>
2024-12-13 12:37:11 +0800

    Initial commit. Base items added to the project.

---
+++ MeinhardtHospitalApp/src/main/java/com/example/controller/HomeController.java
@@ -0,0 +1,20 @@
+package com.example.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("/")
+public class HomeController {
+	
+	@GetMapping("/home")
+	public ModelAndView home() {
+		ModelAndView modelAndView = new ModelAndView("home");
+		modelAndView.addObject("message", "Welcome to Meinhardt Hospital Website!");
+		return modelAndView;
+	}
+
+}
